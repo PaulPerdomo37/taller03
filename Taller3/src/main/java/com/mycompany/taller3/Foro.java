7f86d0ba28cd000a37d9d8edad6b14953fe316a2
Herrera2005 <150067590+Herrera2005@users.noreply.github.com>
2024-10-24 01:13:40 -0500

    Update Foro.java

--- Taller3/src/main/java/com/mycompany/taller3/Foro.java
+++ Taller3/src/main/java/com/mycompany/taller3/Foro.java
@@ -12,4 +12,10 @@ public class Foro {
     private String id;
     private String tematica;
     private int numeroDeComentarios;
+
+    public Foro(String id, String tematica, int numeroDeComentarios) {
+        this.id = id;
+        this.tematica = tematica;
+        this.numeroDeComentarios = numeroDeComentarios;
+    }
 }
